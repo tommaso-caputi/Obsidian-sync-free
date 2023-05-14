6be31590b5ec9a1b8eb2d2fad7d2c1ee9b1b952b
tommaso caputi <tommasocaputi85@gmail.com>
2023-05-14 02:08:14 +0200

    init

---
+++ free-sync/main.ts
@@ -0,0 +1,33 @@
+import { Notice, Plugin } from 'obsidian';
+/* import * as fs from 'fs'; */
+import * as fs from 'fs-extra';
+
+
+// Remember to rename these classes and interfaces!
+
+interface MyPluginSettings {
+	mySetting: string;
+}
+
+
+export default class MyPlugin extends Plugin {
+	settings: MyPluginSettings;
+
+	async onload() {
+
+		// This creates an icon in the left ribbon.
+		const ribbonIconEl = this.addRibbonIcon('github', 'Free Sync', (evt: MouseEvent) => {
+			// Called when the user clicks the icon.
+			copyFile('D:/Obsidian Files/', 'D:/Projects/Github/Obsidian-storage/')
+			new Notice('File synced!');
+		});
+		// Perform additional things with the ribbon
+		ribbonIconEl.addClass('my-plugin-ribbon-class');
+	}
+}
+
+
+
+function copyFile(sourcePath: string, destinationPath: string): void {
+	fs.copy(sourcePath, destinationPath/* , (err) => { } */);
+}
